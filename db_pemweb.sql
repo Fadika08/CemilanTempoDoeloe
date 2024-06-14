6472c23eea1e1b135d5177e317ada4b1b8bd1083
Fadika <116150742+Fadika08@users.noreply.github.com>
2024-06-14 14:12:57 +0700

    Add files via upload

--- db_pemweb.sql
+++ db_pemweb.sql
@@ -1,2 +1,56 @@
-CREATE TABLE akun(
-)`akun`
+/*
+SQLyog Community v13.2.0 (64 bit)
+MySQL - 10.4.32-MariaDB : Database - db_pemweb
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`db_pemweb` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+
+USE `db_pemweb`;
+
+/*Table structure for table `akun` */
+
+DROP TABLE IF EXISTS `akun`;
+
+CREATE TABLE `akun` (
+  `id_akun` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(25) NOT NULL,
+  `NomerHandphone` varchar(14) NOT NULL,
+  PRIMARY KEY (`id_akun`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+/*Data for the table `akun` */
+
+insert  into `akun`(`id_akun`,`username`,`email`,`password`,`NomerHandphone`) values 
+(1,'admin','admin123@gmail.com','admin123','082393654987'),
+(2,'dream','','',''),
+(3,'dream','dream@gmail.com','1234','082393654456');
+
+/*Table structure for table `mitra` */
+
+DROP TABLE IF EXISTS `mitra`;
+
+CREATE TABLE `mitra` (
+  `id_mitra` int(11) NOT NULL AUTO_INCREMENT,
+  `NamaMitra` varchar(25) NOT NULL,
+  `AlamatMitra` varchar(50) NOT NULL,
+  `NomorMitra` varchar(14) NOT NULL,
+  PRIMARY KEY (`id_mitra`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+/*Data for the table `mitra` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
